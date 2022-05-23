c082a9c52c5d4e61ad406ad68d12fd230d30b09d
Jonathan Delgado <hi@jon.soy>
2022-05-23 18:26:44 -0400

    build: update gitignore file

---
+++ env.d.ts
@@ -0,0 +1,12 @@
+
+declare module "node:test" {
+  interface test {
+    (description: string, options: { skip: boolean }, callback: (t: test, done: (error: unknown) => void) => void | Promise<void>): Promise<void>;
+    (description: string, callback: (t: test, done: (error: unknown) => void) => void | Promise<void>): Promise<void>;
+    test: test
+  }
+
+  declare const test: test;
+
+  export = test
+}
